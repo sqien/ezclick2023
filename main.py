71c594260a401c29e7b73c855f3a1e74dcfe5d0a
Oleksandr Rymar <twitchsqien@gmail.com>
2022-12-27 21:27:45 +0100

    quit button added

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+gui = Tk()
+frm = ttk.Frame(gui, padding=10)
+frm.grid()
+ttk.Label(frm, text="EzClicker").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=gui.destroy).grid(column=1, row=0)
+gui.mainloop()
