da0fd03f22b61b336b6bea268884c52de601392a
Oleksandr Rymar <twitchsqien@gmail.com>
2022-12-30 22:14:23 +0100

    Menu almost done

--- commands.py
+++ commands.py
@@ -1,17 +1,12 @@
 import pyautogui
 import time
 import keyboard
+import mouse
 
-cps = int(input("How much clicks do you want? "))
-while True:
-    def click():
-        time.sleep(0.01)
-        pyautogui.click()
+print("Print button left/right")
+wbutton = input()
 
-    def main():
-        if keyboard.is_pressed("ctrl + 1"):
-            for i in range(cps):
-                click()
-                if keyboard.is_pressed("ctrl + 2"):
-                    exit
-    main()
+def click():
+    mouse.click(button=f'{wbutton}')
+
+click()
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import tkinter as tk
+from tkinter import *
+#from tkinter.ttk import *
+#from tkinter import ttk
 
 gui = tk.Tk()
-gui.geometry('400x300')
+var = IntVar()
+gui.title("EzClicker")
+#gui.geometry('400x300')
 gui.resizable(False, False)
-ecl = tk.Label(text="EzClicker").pack()
+#ecl = tk.Label(text="EzClicker").pack()
 
-start = tk.Button(text="Start", width=20, command=gui.destroy).pack()
-quit = tk.Button(text="Quit", width=20, command=gui.destroy).pack()
+hwtext = tk.Label(text="How much cps?").grid(row=0, column=2)
+hwentry = tk.Entry().grid(row=1, column=2)
+start = tk.Button(text="Start", width=20, command=gui.destroy).grid(row=0, column=1)
+wb1 = tk.Radiobutton(text="Left", variable=var, value=1).grid(row=1, column=1)
+wb2 = tk.Radiobutton(text="Right", variable=var, value=2).grid(row=2, column=1)
+quit = tk.Button(text="Quit", width=20, command=gui.destroy).grid(row=7, column=2, padx=5, pady=5)
 
 gui.mainloop()
