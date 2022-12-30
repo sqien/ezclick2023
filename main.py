66f86776ead05c6a8d17cdef46e4ad3e65427621
Oleksandr Rymar <twitchsqien@gmail.com>
2022-12-31 00:04:09 +0100

    v06

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import *
-#from tkinter.ttk import *
-#from tkinter import ttk
 
 gui = tk.Tk()
 var = IntVar()
@@ -10,11 +9,17 @@ gui.title("EzClicker")
 gui.resizable(False, False)
 #ecl = tk.Label(text="EzClicker").pack()
 
-hwtext = tk.Label(text="How much cps?").grid(row=0, column=2)
-hwentry = tk.Entry().grid(row=1, column=2)
-start = tk.Button(text="Start", width=20, command=gui.destroy).grid(row=0, column=1)
+entry_cps = tk.StringVar()
+def test():
+    cps = entry_cps.get()
+    print(cps)
+
+text = tk.Label(text="How much cps?").grid(row=0, column=2)
+entrycps = tk.Entry(textvariable=entry_cps).grid(row=1, column=2)
+start = tk.Button(text="Start", width=20, command=test).grid(row=0, column=1)
 wb1 = tk.Radiobutton(text="Left", variable=var, value=1).grid(row=1, column=1)
 wb2 = tk.Radiobutton(text="Right", variable=var, value=2).grid(row=2, column=1)
 quit = tk.Button(text="Quit", width=20, command=gui.destroy).grid(row=7, column=2, padx=5, pady=5)
+
 
 gui.mainloop()
