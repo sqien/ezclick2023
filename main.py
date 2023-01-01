51ec91c8622f48df7828eafe8bb82172d2a0ebde
Oleksandr Rymar <twitchsqien@gmail.com>
2023-01-01 21:41:56 +0100

    v08, idk how to fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Tk
 from tkinter import *
+from tkinter.ttk import *
 import pyautogui
 import time
 import keyboard
@@ -26,7 +27,7 @@ def test():
     print(button)
 
 #################################
-def clicker():
+def clicker(event):
     global cps
     cps = entry_cps.get()
 
@@ -40,6 +41,7 @@ def clicker():
         for n in cps:
             mouse.click(button='right')
 
+    gui.after(50, lambda event: clicker(event=event))
     #mouse.click(button=left')
 
 #click()
@@ -48,6 +50,7 @@ def clicker():
 text = tk.Label(text="How much cps?").grid(row=0, column=2)
 entrycps = tk.Entry(textvariable=entry_cps).grid(row=1, column=2)
 start = tk.Button(text="Start", width=20, command=clicker).grid(row=0, column=1)
+gui.bind('<Return>', clicker)
 wb1 = tk.Radiobutton(text="Left", variable=var, value=1).grid(row=1, column=1)
 wb2 = tk.Radiobutton(text="Right", variable=var, value=2).grid(row=2, column=1)
 quit = tk.Button(text="Quit", width=20, command=gui.destroy).grid(row=7, column=2, padx=5, pady=5)
